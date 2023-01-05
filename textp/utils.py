e74320f3e932015e5c25997a5b5d83b2d494641e
Gabriel Herrod <gabrielhdezrodriguez@gmail.com>
2023-01-04 20:16:51 -0500

    feat: Visitors improved

--- textp/utils.py
+++ textp/utils.py
@@ -4,3 +4,17 @@ def is_float(anything):
         return True
     except ValueError:
         return False
+
+
+class Singleton():
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        kwargs = kwargs.copy()
+        name = kwargs.get("name", "root")
+        kwargs.pop('name', None)
+        if (cls, name) not in cls._instances:
+            cls._instances[cls, name] = super(
+                Singleton, cls).__call__(
+                *args, **kwargs)
+        return cls._instances[cls, name]
